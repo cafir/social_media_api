0cd2cbb23cf25aca3486395f230f950f3a0295fd
Eca firhan <ecafirhan@gmail.com>
2021-08-31 16:14:45 +0700

    user testing invalid input

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,19 @@ describe User do
                 expect(user.valid?).to eq(true)
             end
         end
+        context "when initialize with invalid input" do
+            it 'should return false' do
+                params = {
+                    'email' => 'ecafirhan@gmail.com',
+                    'bio' => 'plasma'
+                }
+                user = User.new(params)
+                allow(Mysql2::Client).to receive(:new).and_return(double)
+                allow(double).to receive(:query).with(
+                    "select id from users where email = '#{user.email}' or username = '#{user.username}'"
+                ).and_return([])
+                expect(user.valid?).to eq(false)
+            end
+        end
     end
 end
